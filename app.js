688e8276a3cf54c76a0bb32975d7d9efd2efdd04
offghm <99176790+offghm@users.noreply.github.com>
2022-06-23 18:51:53 +0300

    JavaScript project 3

---
+++ app.js
@@ -0,0 +1,44 @@
+const upBtn = document.querySelector(".up-button");
+const downBtn = document.querySelector(".down-button");
+const sidebar = document.querySelector(".sidebar");
+const container = document.querySelector(".container");
+const mainSlide = document.querySelector(".main-slide");
+const slideCount = mainSlide.querySelectorAll("div").length;
+let activeSlideIndex = 0;
+// для совпадения картинок и сайдабара -300vh
+sidebar.style.top = `-${(slideCount - 1) * 100}vh`;
+
+upBtn.addEventListener("click", () => {
+  changeSlide("up");
+});
+
+downBtn.addEventListener("click", () => {
+  changeSlide("down");
+});
+// Что бы работало на кнопки
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowUp") {
+    changeSlide("up");
+  } else if (event.key === "ArrowDown") {
+    changeSlide("down");
+  }
+});
+
+function changeSlide(direction) {
+  if (direction === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex === slideCount) {
+      activeSlideIndex = 0;
+    }
+  } else if (direction === "down") {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slideCount - 1;
+    }
+  }
+
+  const height = container.clientHeight;
+
+  mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
+  sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
+}
